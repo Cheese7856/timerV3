d262b5bdc195515ed3b0ab5d622f05e1c87cf44b
Cheese7856 <elander.olsen@gmail.com>
2025-08-15 08:25:15 +0000

    Dag 1 ferdig

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+
+
+setTimeout(function () {
+    // Code to be executed after 1 second
+    document.querySelector("#cp").shadowRoot.querySelector("svg > text").innerHTML = "Ostepop"
+}, 1000);
